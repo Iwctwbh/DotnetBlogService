e5f59baa74db6f88d220e7960121290797729b27
夜夜三倍速 <iwctwbh@gmail.com>
2023-06-20 10:24:41 +0800

    Update JsonFormat

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 using DotnetBlogService.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MySqlConnector;
 using Newtonsoft.Json.Linq;
 using System.Text;
+using System.Text.Json;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -43,7 +43,12 @@ builder.Services.AddPooledDbContextFactory<BlogContext>(optionsBuilder =>
     optionsBuilder.UseMySql(builder.Configuration.GetConnectionString("ConnectionStrings:DefaultConnection"),
         ServerVersion.Parse("10.6.5-mariadb")));
 
-WebApplication app = builder.Build();
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+{
+    options.SerializerOptions.PropertyNamingPolicy = null;
+});
+
+var app = builder.Build();
 app.UseCors("Policy1");
 //app.UseEndpoints(e => { });
 //app.Use((context, next) => next(context));
@@ -64,8 +69,8 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/AddPost", (string argTitle, string argContent) =>
 {
-    using BlogContext db = new BlogContext();
-    TblPost post = new TblPost
+    using var db = new BlogContext();
+    var post = new TblPost
     {
         Title = argTitle,
         Content = argContent,
@@ -81,10 +86,10 @@ app.MapGet("/AddPost", (string argTitle, string argContent) =>
 
     //var result = BlogContext.FromSqlRaw("EXECUTE MyStoredProc @param1, @param2", param1, param2).ToList();
 
-    using BlogContext context = new BlogContext();
-    MySqlParameter parameter0 = new MySqlParameter("@inputParam", "someValue");
-    MySqlParameter parameter1 = new MySqlParameter("@inputParam", "someValue");
-    MySqlParameter parameter2 = new MySqlParameter("@inputParam", "someValue");
+    using var context = new BlogContext();
+    var parameter0 = new MySqlParameter("@inputParam", "someValue");
+    var parameter1 = new MySqlParameter("@inputParam", "someValue");
+    var parameter2 = new MySqlParameter("@inputParam", "someValue");
 
     List<TblPost> result = context.TblPosts.FromSqlRaw("CALL sp_AddPost({0}, {1}, {2})", parameter0, parameter1, parameter2)
         .ToList();
@@ -94,25 +99,25 @@ app.MapGet("/AddPost", (string argTitle, string argContent) =>
 
 var maxMessageSize = 80 * 1024;
 
+
 app.MapPost("/GetPosts", async (HttpRequest req, Stream argBody, BlogContext db) =>
 {
-    var sqlResult = db.TblPosts.AsNoTracking();
-    var defaultResult = await sqlResult.Where(w => w.IsActive == 1).Skip(0).Take(10).ToListAsync();
+    var skip = 0;
 
-    if (req.ContentLength is not null && req.ContentLength > maxMessageSize) return defaultResult;
+    if (req.ContentLength is not null && req.ContentLength > maxMessageSize) return await db.TblPosts.AsNoTracking().Where(w => w.IsActive == 1).Skip(0).Take(10).ToListAsync();
 
-    int readSize = (int?)req.ContentLength ?? maxMessageSize + 1;
+    var readSize = (int?)req.ContentLength ?? maxMessageSize + 1;
 
-    byte[] buffer = new byte[readSize];
+    var buffer = new byte[readSize];
 
-    int read = await argBody.ReadAtLeastAsync(buffer, readSize, false);
-    if (read > maxMessageSize) return defaultResult;
-    string bufferString = Encoding.Default.GetString(buffer);
+    var read = await argBody.ReadAtLeastAsync(buffer, readSize, false);
+    if (read > maxMessageSize) return await db.TblPosts.AsNoTracking().Where(w => w.IsActive == 1).Skip(0).Take(10).ToListAsync();
+    var bufferString = Encoding.Default.GetString(buffer);
 
     bufferString = System.Text.RegularExpressions.Regex.Unescape(bufferString);
     bufferString = bufferString.Trim('"');
 
-    JObject jsonBody = new JObject();
+    var jsonBody = new JObject();
     try
     {
         jsonBody = JObject.Parse(bufferString);
@@ -124,10 +129,12 @@ app.MapPost("/GetPosts", async (HttpRequest req, Stream argBody, BlogContext db)
 
     // value为数字时，长度过长直接报错
     //var c = jsonBody.Value<string>("skip");
-    Int32.TryParse(jsonBody.Property("skip")?.Value.ToString(), out Int32 skip);
+    int.TryParse(jsonBody.Property("skip")?.Value.ToString(), out skip);
     skip = Math.Max(0, skip);
 
-    return await sqlResult.Where(w => w.IsActive == 1).Skip(skip).Take(10).ToListAsync();
+    var a = await db.TblPosts.AsNoTracking().Where(w => w.IsActive == 1).Skip(skip).Take(10).ToListAsync();
+    var b = JsonSerializer.Serialize(a);
+    return a;
 });
 
 //app.MapPost("/register", async (HttpRequest req, Stream body,
